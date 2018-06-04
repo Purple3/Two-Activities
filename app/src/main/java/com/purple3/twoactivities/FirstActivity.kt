161db1e73c63496789aa8c40247d44079276cf6e
Purple3 <Munni1992$>
2018-06-03 17:23:08 -0700

    Tried changing launcher activity

--- app/src/main/java/com/purple3/twoactivities/FirstActivity.kt
+++ app/src/main/java/com/purple3/twoactivities/FirstActivity.kt
@@ -8,9 +8,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
-
-
-
 class FirstActivity : AppCompatActivity() {
 
     private lateinit var btnForNextPage : Button
